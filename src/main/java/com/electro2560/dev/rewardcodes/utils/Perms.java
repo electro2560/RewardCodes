b3172827aa69490ca5406cfe8be52d69fa3725aa
Mitchell Sulkowski <arduinoboy2560@gmail.com>
2020-04-17 23:02:13 -0400

    Update permission

--- src/main/java/com/electro2560/dev/rewardcodes/utils/Perms.java
+++ src/main/java/com/electro2560/dev/rewardcodes/utils/Perms.java
@@ -11,7 +11,7 @@ public class Perms {
 	public static final String canUseCommand = "rewardcodes.redeem";
 	public static final String isAdmin = "rewardcodes.admin";
 	public static final String receiveAll = "rewardcodes.receive.*";
-	public static final String isCheckForUpdates = "rewardcodes.canCheckForUpdates";
+	public static final String isCheckForUpdates = "rewardcodes.checkforupdates";
 	
 	public static final String receiveBase = "rewardcodes.receive.";
 	
